a7d80b0e818cf90b5a44a40110d949c70f3c3906
juliojun2023 <julio.jun@pwd.net.br>
2023-06-01 15:27:37 -0300

    Add project files.

---
+++ JunSystem_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JunSystem_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("JunSystem_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8D7779ED-D0F9-4472-A579-B57E94FF9EC4")]
+[assembly: AssemblyVersion("1.0.0.0")]
