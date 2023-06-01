2112ebedc5a2588b8bbe3fddf5e81db4b0ccefaf
juliojun2023 <julio.jun@pwd.net.br>
2023-06-01 15:55:01 -0300

    Version inicial

---
+++ ToDelete_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ToDelete_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("ToDelete_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("57B048BD-FF0B-433C-B7F8-0D92AD0B0447")]
+[assembly: AssemblyVersion("1.0.0.0")]
